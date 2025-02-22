e79416e9c2230c5f8ac79e4141e08a50e6c2eed5
Josh Fried <joshuafried@gmail.com>
2025-02-22 15:10:29 -0500

    apply patch script

---
+++ src/internal/syscall/unix/pidfd_junction.go
@@ -0,0 +1,16 @@
+//go:build junction
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package unix
+
+import "syscall"
+
+func PidFDSendSignal(pidfd uintptr, s syscall.Signal) error {
+	_, _, errno := syscall.Syscall(pidfdSendSignalTrap, pidfd, uintptr(s), 0)
+	if errno != 0 {
+		return errno
+	}
+	return nil
+}
--- src/runtime/defs_linux_amd64.go
+++ src/runtime/defs_linux_amd64.go
@@ -1,3 +1,4 @@
+//go:build !junction
 // created by cgo -cdefs and then converted to Go
 // cgo -cdefs defs_linux.go defs1_linux.go
 
